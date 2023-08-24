b318dddec58fb00a3afdacb2afe35e9fb662546f
Gustavo Guimarães <47610524+GGustavoGG@users.noreply.github.com>
2023-08-24 20:19:46 -0300

    base projeto

---
+++ src/main/java/com/crypto/cryptostrategyfactory/CryptoStrategyFactoryApplication.java
@@ -0,0 +1,13 @@
+package com.crypto.cryptostrategyfactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoStrategyFactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoStrategyFactoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crypto/cryptostrategyfactory/CryptoStrategyFactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crypto.cryptostrategyfactory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoStrategyFactoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
